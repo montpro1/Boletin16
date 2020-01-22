bf154c1f2b6958958046eb0748e6ab1b23b4fa39
DANIELCASTELAO\vmontestovar <vmontestovar@danielcastelao.org>
2020-01-22 11:02:01 +0100

    gnr

---
+++ src/boletin16/Boletin16.java
@@ -0,0 +1,15 @@
+
+package boletin16;
+
+
+public class Boletin16 {
+
+   
+    public static void main(String[] args) {
+       Persoa tenor=new Persoa();
+       Canario canar=new Canario();
+   tenor.cantar();
+   canar.cantar();
+    
+}
+}
